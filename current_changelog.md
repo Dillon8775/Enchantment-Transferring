971f1390bd6fac34192af748ca56affa79ddbfc4
Dillon <dilloneystrickland12@gmail.com>
2026-09-19 14:51:28 -0400

    update to dillonlib 1.2

--- current_changelog.md
+++ current_changelog.md
@@ -1,15 +1,2 @@
-# Version 1.1
-
-## Now requires [DillonLib](https://modrinth.com/mod/dillon-lib) in order to run.
-
-## Changes
-- New texture for enchantment transferrer block.
-- New interface for the enchantment transferrer.
-    - Transferring enchantments has been optimized.
-    - You can now transfer enchantments directly to empty books.
-    - Add a diamond to negate durability damage from the tool.
-    - Purely based off of the [Speedrunner Mod](https://modrinth.com/mod/speedrunner-mod).
-- The enchantment transferrer is now considered a "block entity".
-- Fixed bug where using enchantment transferrer sometimes opens the crafting table GUI.
-- Now requires Fabric Loader 0.19.3 or greater to load.
-- Backend optimizations.
+# Version 1.1.1
+- Now requires [DillonLib](https://modrinth.com/mod/dillon-lib) 1.2.
--- src/main/java/net/dillon/enchantmenttransferring/platform/EtPlatform.java
+++ src/main/java/net/dillon/enchantmenttransferring/platform/EtPlatform.java
@@ -2,12 +2,9 @@ package net.dillon.enchantmenttransferring.platform;
 
 import net.dillon.dillonlib.platform.ModPlatform;
 import net.dillon.dillonlib.platform.Platforms;
-import net.dillon.dillonlib.platform.info.LogoWidth;
-import net.dillon.dillonlib.platform.info.PlatformName;
-import net.dillon.dillonlib.platform.info.PlatformRelease;
+import net.dillon.dillonlib.platform.info.Platform;
+import net.dillon.dillonlib.platform.info.Release;
 import net.dillon.enchantmenttransferring.helper.ModConstants;
-import org.jetbrains.annotations.NotNull;
-import org.slf4j.Logger;
 
 public class EtPlatform extends ModPlatform {
 
@@ -17,27 +14,17 @@ public class EtPlatform extends ModPlatform {
     }
 
     @Override
-    public @NotNull Logger logger() {
-        return ModConstants.LOGGER;
-    }
-
-    @Override
     public String modVersion() {
         return Platforms.getCommonPlatform().commonModVersion(ModConstants.MOD_ID);
     }
 
     @Override
-    public @NotNull PlatformName platformName() {
-        return PlatformName.FABRIC;
+    public Release release() {
+        return Release.STABLE;
     }
 
     @Override
-    public @NotNull PlatformRelease platformRelease() {
-        return PlatformRelease.STABLE;
-    }
-
-    @Override
-    public @NotNull LogoWidth logoWidth() {
-        return LogoWidth.DEFAULT;
+    public Platform platform() {
+        return Platform.FABRIC;
     }
 }
